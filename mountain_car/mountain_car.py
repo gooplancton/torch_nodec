d9e69bf2597c2085ce73ca2fbee9ab967afbff3b
gooplancton <gooplancton@outlook.com>
2023-02-17 03:29:17 +0100

    fixed mountain car dynamics

--- mountain_car/mountain_car.py
+++ mountain_car/mountain_car.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 import pytorch_lightning as pl
-from torchdyn.nn import DepthCat
 
 from nodec import ControlLearner, ControlledSystem, ControlLoss
 
@@ -15,13 +14,15 @@ class MountainCarLoss(ControlLoss):
 
 
 class MountainCar(ControlledSystem):
-    def __init__(self, controller: nn.Module):
+    def __init__(self, controller: nn.Module, m: float=1e3, g: float=2.5e-3):
+        self.m = m
+        self.g = g
         super().__init__(controller, torch.tensor([[-0.6, 0.4], [0.0, 0.0]]).float())
 
     def dynamics(self, t: torch.Tensor, x: torch.Tensor, control: torch.Tensor) -> torch.Tensor:
-        clipped_control = torch.clamp(control, -1, 1)
+        
         position, velocity = x[:, :0], x[:, 1:]
-        acceleration = 0.0015*clipped_control - 0.025*torch.cos(3*position)
+        acceleration = 1/self.m * control - self.g * torch.sin(torch.pi + 3*position)
         x_prime = torch.cat([velocity, acceleration], axis=1)
 
         return x_prime
